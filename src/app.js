cd5cfc17f7d3c3c5f309fbfd52133f1fb7a918e7
Abhishek Srivastava <abhi.ss9636@gmail.com>
2026-07-11 05:25:54 +0530

    added app constants db .js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    return res.json("good")
+})
+
+app.listen(PORT, () => {
+    console.log(`app running at PORT: ${PORT}`)
+})
