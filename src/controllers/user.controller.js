5a6e65a02d3024389ba19967e5aad66f25770765
Abhishek Srivastava <abhi.ss9636@gmail.com>
2026-07-14 17:59:31 +0530

    logout route and its controller added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { User } from '../models/user.model.js';
 const registerUser = async (req, res) => {
     try {
         const { username, password } = req.body;
-        console.log('request body for account creation/register: ', req.body);
+        console.log('\nAccount creation reuqested for username: ', username);
 
         // Basic backend validation
         if(!username || !password){
@@ -30,7 +30,7 @@ const registerUser = async (req, res) => {
         res.status(500).json({message: 'Internal server Error occured while creating new user account', error: error.message});
     
     } finally {
-        console.log('Accont creation service was triggered');
+        console.log('Accont creation service was triggered !!!');
     }
 
 }
@@ -39,17 +39,21 @@ const loginUser = async (req, res) => {
     try {
         // checking if user already exists 
         const { username, password } = req.body;
+        console.log('\nLog In requested for username: ', username);
 
         const user = await User.findOne({
             username: username.toLowerCase()
         });
-        console.log('Find User: ', user);
+        console.log('Found User in DB? : ', user);
         // no users exists return with message
-        if(!user) return res.status(400).josn({message: 'Username does not exists !!'})
+        if(!user) return res.status(400).josn({
+            message: 'Username does not exists !!'
+        });
             
         // compare Passwords
         const isMatch = await user.comparePassword(password);
         console.log('isMatch: ', isMatch);
+
         if(!isMatch) return res.status(400).json({
             message: 'Invalid password !!!'
         });
@@ -64,7 +68,8 @@ const loginUser = async (req, res) => {
         
     } catch (error) { 
         res.status(500).json({
-            message: 'Internal Server error occured while Logging In', error: error.message
+            message: 'Internal Server error occured while Logging In',
+            error: error
         });
 
         console.log('Error occured !!!', error);
@@ -74,4 +79,36 @@ const loginUser = async (req, res) => {
     }
 }
 
-export { registerUser, loginUser };
+const logoutUser = async (req, res) => {
+    try {
+        const { username } = req.body;
+        console.log('\nLog out requested for username: ', username);
+        const user = await User.findOne({
+            username
+        });
+        
+        console.log('username found in DB? : ', user);
+        
+        if(!user) return res.status(404).json({
+            message: 'Usernmae does not exists for logout request'
+        });
+
+        res.status(200).json({
+            message: 'User logged out successfully'
+        });
+        
+        
+    } catch (error) {
+        
+        res.status(500).json({
+            message: 'Internal server error occured, while logging out !!!',
+            error: error
+        });
+    } finally {
+        console.log('Log out request was made !!!');
+
+    }
+    
+}
+
+export { registerUser, loginUser, logoutUser };
