18bcd671134fdf3d680cee5bfb3bb712768c73c7
Abhishek Srivastava <abhi.ss9636@gmail.com>
2026-07-14 14:49:49 +0530

    login route and its loginUser controller added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { User } from '../models/user.model.js';
 const registerUser = async (req, res) => {
     try {
         const { username, password } = req.body;
-        console.log('request body: ', req.body);
+        console.log('request body for account creation/register: ', req.body);
 
         // Basic backend validation
         if(!username || !password){
@@ -27,12 +27,51 @@ const registerUser = async (req, res) => {
         res.status(201).json({message: 'Account created successfully !!! ', userName: user.username });
 
     } catch (error) {
-        res.status(500).json({message: 'Internal server Error occured while creating new user account', error});
+        res.status(500).json({message: 'Internal server Error occured while creating new user account', error: error.message});
     
     } finally {
-        console.log('Accont creation service triggered');
+        console.log('Accont creation service was triggered');
     }
 
 }
 
-export { registerUser };
+const loginUser = async (req, res) => {
+    try {
+        // checking if user already exists 
+        const { username, password } = req.body;
+
+        const user = await User.findOne({
+            username: username.toLowerCase()
+        });
+        console.log('Find User: ', user);
+        // no users exists return with message
+        if(!user) return res.status(400).josn({message: 'Username does not exists !!'})
+            
+        // compare Passwords
+        const isMatch = await user.comparePassword(password);
+        console.log('isMatch: ', isMatch);
+        if(!isMatch) return res.status(400).json({
+            message: 'Invalid password !!!'
+        });
+
+        res.status(200).json({
+            message: 'User logged in successfully !!!',
+            user: {
+                id: user._id,
+                username: user.username
+            }
+        });
+        
+    } catch (error) { 
+        res.status(500).json({
+            message: 'Internal Server error occured while Logging In', error: error.message
+        });
+
+        console.log('Error occured !!!', error);
+        
+    } finally {
+        console.log('Log In was attempted !!!');
+    }
+}
+
+export { registerUser, loginUser };
