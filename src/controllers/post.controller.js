4c7d610f65c20436a707f4724b2c600b5fac5b4d
Abhishek Srivastava <abhi.ss9636@gmail.com>
2026-07-14 19:53:10 +0530

    "Routes and controllers added for get all posts for a given name"

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -29,4 +29,40 @@ const createPost = async (req, res) => {
     }
 }   
 
-export { createPost };
+// fetch all posts of a given 'name'
+const getAllPosts = async (req, res) => {
+    try {
+        const { name } = req.body;
+        console.log('\nGet all posts requested for: ', name);
+
+        // Basic backend validation of request credentials
+        if(!name) return res.status(400).json({
+            message: 'name could not be empty in getAllPosts request'
+        });
+
+        // fetch all posts from DB
+        const allPosts = await Post.find({ name });
+        console.log('Number of posts found', allPosts.length );
+        
+        // if(!allPosts) // This is wrong and will not work because, even empty array [] is 'truthy' value
+        if(allPosts.length === 0)
+        {
+            res.status(400).json({
+                message: 'No post/s is found'
+            });
+        }      
+
+        res.status(200).json({ allPosts: allPosts});
+        
+    } catch (error) {
+        res.status(500).json({
+            message: 'ERROR: Could not fetch all posts',
+            error: error.message
+        });
+        
+    } finally {
+        console.log('Get all posts request was made !!!');
+    }
+}
+
+export { createPost, getAllPosts};
--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { createPost } from '../controllers/post.controller.js';
+import { getAllPosts } from '../controllers/post.controller.js';
 
 const router = Router();
 
 router.route('/create').post(createPost);
+router.route('/getAllPosts').get(getAllPosts)
 
 export default router;
